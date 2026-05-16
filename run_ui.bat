2bcd94af18492c533784dc213c93e84f13584e34
Amit Kumar Singh <kumaramit.14singh@gmail.com>
2026-05-16 21:23:55 +0530

    Done change in code

--- run_ui.bat
+++ run_ui.bat
@@ -1,4 +1,4 @@
 @echo off
 call ".venv\Scripts\activate.bat"
-python ui_final.py
+python app.py
 pause
--- video_view.py
+++ video_view.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 from ultralytics import YOLO
 
@@ -8,12 +9,13 @@ def process_video(
         save:bool = False,
     ) -> str:
     # Load the YOLOv8 model
-    model = YOLO(r'waste_detection\yolo\weights\best.pt')
-    results = model(video_path, save=save,show=show, imgsz=size)
-    dir =  results[0].save_dir
+    model_path = os.path.join('waste_detection', 'yolo', 'weights', 'best.pt')
+    model = YOLO(model_path)
+    results = model(video_path, save=save, show=show, imgsz=size)
+    dir = results[0].save_dir
     del results
     return dir
 
 if __name__ == '__main__':
-    video_path = r'E:\project_24\Yoloclassification Projects\video\waste.mp4'
+    video_path = os.path.join('video', 'waste.mp4')
     print(process_video(video_path))
